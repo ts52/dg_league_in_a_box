7daab604b7d02c6c35d8331679ef85402454a6c1
Tim Seiger <tim.seiger@gmail.com>
2016-02-26 04:29:16 +0000

    still working on player check in

--- check_in_player.php
+++ check_in_player.php
@@ -6,7 +6,7 @@
   $playerid = $_POST['playerid'];
   $course = $_POST['course'];
 
-  $player_query = "SELECT * from players WHERE playerid is :playerid";
+  $player_query = "SELECT * from players WHERE playerid IS :playerid";
   $pq_stmt = $db->prepare($player_query);
   $pq_stmt->bindParam(":playerid",$playerid);
   $pq_ret = $pq_stmt->execute();
@@ -17,17 +17,39 @@
     $pool = $row['pool'];
   }
 
+  $checked_in_player_query = "SELECT * from scores WHERE week IS :week AND course IS :course"
+  $cipq_stmt = $db->prepare($checked_in_player_query);
+  $cipq_stmt->bindParam(":week",$week);
+  $cipq_stmt->bindParam(":course",$course);
+  $cipq_ret = $cipq_stmt->execute();
+  $row_count = 0;
+  while ($row = $cipq_ret->ftechArray(SQLITE3_ASSOC) ){
+    $row_count++;
+  }
+
+  if ($course == 'hill'){
+    $start_hole = $hill_start_array[$row_count];
+  }elseif ($course == 'general'){
+    $start_hole = $general_start_array[$row_count];
+  }
+
 	$insert_sql = <<<EOF
 		INSERT INTO scores 
-		    (playerid,lastname,firstname,pool)
+		    (playerid,lastname,firstname,pool,week,course,incoming_tag,start_hole)
 		    VALUES
-		    (:playerid,:lastname,:firstname,:pool);
+		    (:playerid,:lastname,:firstname,:pool,:week,:course,:incoming_tag,:start_hole);
 EOF;
 	$add_player_stmt = $db->prepare($insert_sql);
 
+	$add_player_stmt->bindParam(":playerid", $playerid);
 	$add_player_stmt->bindParam(":lastname", $lastname);
 	$add_player_stmt->bindParam(":firstname", $firstname);
 	$add_player_stmt->bindParam(":pool", $pool);
+	$add_player_stmt->bindParam(":week", $week);
+	$add_player_stmt->bindParam(":course", $course);
+	$add_player_stmt->bindParam(":incoming_tag", $incoming_tag);
+	$add_player_stmt->bindParam(":start_hole", $start_hole);
 	$add_player_stmt->execute();
-	print ("Player $firstname $lastname added to $pool pool<br>");
+	print ("Player $firstname $lastname checked in to the $course<br>");
+  print ("Your start hole is $start_hole.<br>");
 ?>
--- get_config.php
+++ get_config.php
@@ -1,22 +1,24 @@
 <?php
   // Before including this php file
   // set $db_file, and include db_setup.php
-	$week = "";
+	$week = -1;
 	$system_state = "";
-  $hill_start_order = "";
-  $general_start_order = "";
-	$amount_to_payout = "";
-	$amount_to_ace_pot = "";
-	$amount_to_course = "";
-	$amount_to_bonanza = "";
-	$a_pool_payout_count = "";
-	$b_pool_payout_count = "";
-	$c_pool_payout_count = "";
-	$w_pool_payout_count = "";
-	$a_pool_handicap = "";
-	$b_pool_handicap = "";
-	$c_pool_handicap = "";
-	$w_pool_handicap = "";
+  $hill_start_order = "1,2,3,5,6,11,13,14,18,17,1,2,3,5,6,11,13,14,18,17,1,2,3,5,6,11,13,14,18,17,1,2,3,5,6,11,13,14,18,17,10,10,10,10,12,12,12,12,15,15,15,15,16,16,16,16,4,4,4,4,9,9,9,9,8,8,8,8,7,7,7,7";
+  $hill_max_players = 72;
+  $general_start_order = "1,1,1,1,14,14,14,14,12,12,12,12,9,9,9,9,1,1,1,1,12,12,12,12,9,9,9,9,1,1,1,1,9,9,9,9";
+  $general_max_players = 36;
+	$amount_to_payout = 2.25;
+	$amount_to_ace_pot = 0.75;
+	$amount_to_course = 1;
+	$amount_to_bonanza = 1;
+	$a_pool_payout_count = 5;
+	$b_pool_payout_count = 5;
+	$c_pool_payout_count = 5;
+	$w_pool_payout_count = 3;
+	$a_pool_handicap = 6;
+	$b_pool_handicap = 6;
+	$c_pool_handicap = 6;
+	$w_pool_handicap = 6;
 	$config_query = "SELECT * from config;";
 	$cq_ret = $db->query($config_query);
 	$row_count = 0;
@@ -39,4 +41,6 @@
 		$c_pool_handicap = $row['c_pool_handicap'];
 		$w_pool_handicap = $row['w_pool_handicap'];
 	}
+  $hill_start_array = explode ( ',', $hill_start_order );
+  $general_start_array = explode ( ',', $general_start_order );
 ?>
