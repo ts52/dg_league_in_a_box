737212baf0e90668ffbebfd2b81c2f41f8034121
Tim Seiger <tim.seiger@gmail.com>
2016-03-25 08:27:43 +0000

    adding notion of the course being full, showing how full at check in

--- index.php
+++ index.php
@@ -7,11 +7,43 @@
 	include './db_setup.php';
   include './get_config.php';
   if ($system_state == 'check_in_open') {
+print "<!-- DEBUG check in open -->\n";
+    $player_query = "SELECT * from scores WHERE week IS :week ;";
+    $hill_player_count = $general_player_count = 0;
+    $taken_holes = array();
+    $pq_stmt = $db->prepare($player_query);
+    $pq_stmt->bindParam(":week",$week);
+print "<!-- DEBUG set up for player counting -->\n";
+    $pq_ret = $pq_stmt->execute();
+print "<!-- DEBUG after SQL query -->\n";
+    while ( $row = $pq_ret->fetchArray(SQLITE3_ASSOC) ) {
+      if ($row['course'] == 'hill'){
+print "<!-- DEBUG counting player for the hill -->\n";
+        $hill_player_count++;
+      } elseif ($row['course'] == 'general') {
+print "<!-- DEBUG counting player for the general -->\n";
+        if ($row['start_hole'] == "W") {
+print "<!-- DEBUG counting player for the general waiting area -->\n";
+          $general_player_count++;
+        } else {
+print "<!-- DEBUG counting player for the general hole {$row['start_hole']} -->\n";
+          $taken_holes[$row['start_hole']] = TRUE;
+        }
+      }
+    }
+print "<!-- DEBUG after count loop -->\n";
+    foreach ( $taken_holes as $hole => $value ) {
+      print "<!-- DEBUG: general_hole $hole is already taken -->\n";
+      $general_player_count += 4;
+    }
     print "<h3>Check in</h3>";
     print "Current week is $week<br>";
+    print "The Hill is $hill_player_count/$hill_max_players full.<br>\n";
+    print "The General is $general_player_count/$general_max_players full.<br>\n";
+    print "<br>\n";
     print "<form action='check_in_search.php' method='post'>";
-    print "<input type='radio' name='course' value='hill' required>The Hill</input>  ";
-    print "<input type='radio' name='course' value='general' required>The General</input><br>";
+    if ($hill_player_count < $hill_max_players) print "<input type='radio' name='course' value='hill' required>The Hill</input><br>";
+    if ($general_player_count < $general_max_players) print "<input type='radio' name='course' value='general' required>The General</input><br>";
     print "Last Name: <input type='text' name='lastname' required><br>";
     print ('<input type="submit" value="Submit">');
     print ('</form>');
