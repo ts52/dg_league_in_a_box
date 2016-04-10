749d5e07d8b0a8c0c33e69c43402df495150ea14
Tim Seiger <tim.seiger@gmail.com>
2016-04-10 04:59:38 +0000

    adding read back from db to confirm player check in

--- index.php
+++ index.php
@@ -301,28 +301,46 @@ EOF;
 					$add_player_stmt->bindParam(":start_hole", $start_hole);
 					$add_player_stmt->execute();
 
-					print "<!-- Checking how many players already on hole $start_hole on the $course -->\n";
-					$hole_query = "SELECT * from scores WHERE week IS :week AND course IS :course AND start_hole IS :start_hole";
-					$hq_stmt = $db->prepare($hole_query);
-					$hq_stmt->bindParam(":week", $week);
-					$hq_stmt->bindParam(":course", $course);
-					$hq_stmt->bindParam(":start_hole", $start_hole);
-					$hq_ret = $hq_stmt->execute();
-					$player_count = 0;
-					while($row = $hq_ret->fetchArray(SQLITE3_ASSOC) ){
-						$player_count++;
+					print "<!-- DEBUG: Confirming check in by reading back from db -->\n";
+					$player_query = "SELECT * from scores WHERE week IS :week AND playerid IS :playerid ;";
+					$pq_stmt = $db->prepare($player_query);
+					$pq_stmt->bindParam(":week", $week);
+					$pq_stmt->bindParam(":playerid", $playerid);
+					$pq_ret = $pq_stmt->execute();
+					$row_count = 0;
+					while ($row = $pq_ret->fetchArray(SQLITE3_ASSOC)){
+						$row_count++;
+						$course = $row['course'];
+						$lastname = $row['lastname'];
+						$firstname = $row['firstname'];
+						$start_hole = $row['start_hole'];
 					}
-					print "<!-- $player_count players are on this hole. -->\n";
+					if ($row_count != 1){
+						print "<h2>Something went wrong</h2>\nPlease see Luke or Tim\n";
+					}else{
+						print "<!-- Checking how many players already on hole $start_hole on the $course -->\n";
+						$hole_query = "SELECT * from scores WHERE week IS :week AND course IS :course AND start_hole IS :start_hole";
+						$hq_stmt = $db->prepare($hole_query);
+						$hq_stmt->bindParam(":week", $week);
+						$hq_stmt->bindParam(":course", $course);
+						$hq_stmt->bindParam(":start_hole", $start_hole);
+						$hq_ret = $hq_stmt->execute();
+						$player_count = 0;
+						while($row = $hq_ret->fetchArray(SQLITE3_ASSOC) ){
+							$player_count++;
+						}
+						print "<!-- $player_count players are on this hole. -->\n";
 
-					print ("Player $firstname $lastname checked in to the $course<br>\n");
-					if ($course == "hill") print ("<h2>Your start hole is $start_hole.</h2>\n");
-					if ($player_count == 1 and $course == "hill") {
-						print ("<h3>Please put your tag on the board, pay $pay_amount and get a scorecard.</h3>\n");
-					} else {
-						print ("Please put your tag on the board and pay $pay_amount.<br>\n");
-					}
-					if ($course == "general"){
-						print "<br><a href='form_general_card.php'>Find other players waiting for a card on the General</a>\n";
+						print ("Player $firstname $lastname checked in to the $course<br>\n");
+						if ($course == "hill") print ("<h2>Your start hole is $start_hole.</h2>\n");
+						if ($player_count == 1 and $course == "hill") {
+							print ("<h3>Please put your tag on the board, pay $pay_amount and get a scorecard.</h3>\n");
+						} else {
+							print ("Please put your tag on the board and pay $pay_amount.<br>\n");
+						}
+						if ($course == "general"){
+							print "<br><a href='form_general_card.php'>Find other players waiting for a card on the General</a>\n";
+						}
 					}
 				}
 			}
